45b35b85bac8ae842bd84f754b825a2294ab5eea
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-03-29 16:05:21 +0100

    Cross finished

--- app/CFOP/CFOP.hs
+++ app/CFOP/CFOP.hs
@@ -1,17 +1,19 @@
 module CFOP.CFOP where
 
 import Cube
+import CFOP.Cross
 import CFOP.PLL
 import CFOP.OLL
 import CFOP.F2L
 
 solve :: Cube Algorithm
 solve = do
+    crossMoves <- cross
     f2lMoves <- f2l
     ollMoves <- oll
     pllMoves <- pll
     aufMove <- auf
-    return $ removeCancellingMoves (f2lMoves ++ ollMoves ++ pllMoves ++ aufMove) []
+    return $ removeCancellingMoves (crossMoves ++ f2lMoves ++ ollMoves ++ pllMoves ++ aufMove) []
 
 auf :: Cube Algorithm
 auf = do
