9d8cb09be23000edbdb00244836debe7d9352a3d
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-04-15 13:20:21 +0200

    Generate pdf solution from moves on bluetooth cube

--- app/BluetoothCube.hs
+++ app/BluetoothCube.hs
@@ -3,6 +3,12 @@ module BluetoothCube (bluetooth) where
 import PDFCube (generatePDFFromCubeState)
 import System.IO
 import System.Process
+import Cube (move, Move)
+import CubeParser (parseMove)
+import qualified Data.Text as T
+import Text.Megaparsec (runParser)
+import CubeState (CubeState, solvedCube)
+import Control.Monad.State (execState)
 
 
 bluetooth :: IO ()
@@ -20,8 +26,27 @@ bluetooth = do
 
 
 readLines :: Int -> Handle -> IO ()
-readLines 25 _ = return ()
+readLines 25 hout = scrambleCube 0 hout solvedCube
 readLines count hout = do
     input <- hGetLine hout
     putStrLn input
     readLines (count + 1) hout
+
+scrambleCube :: Int -> Handle -> CubeState -> IO ()
+scrambleCube 100 _ _ = return ()
+scrambleCube count hout cubeState = do
+    m <- getNextMove hout
+    let newCubeState = nextState m cubeState
+    generatePDFFromCubeState newCubeState
+    scrambleCube (count + 1) hout newCubeState
+
+nextState :: Move -> CubeState -> CubeState
+nextState m = execState (move m)
+
+getNextMove :: Handle -> IO Move
+getNextMove hout = do    
+    input <- hGetLine hout
+    let eitherMove = runParser parseMove "" (T.pack input)
+    case eitherMove of
+        Right m -> return m
+        Left errorMessage -> error $ show errorMessage
