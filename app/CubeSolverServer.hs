9898bbd4930078c9b62fe9545a0e05159ecd49b9
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-04-15 22:39:50 +0200

    Return solution of input cube in api

--- app/CubeSolverServer.hs
+++ app/CubeSolverServer.hs
@@ -6,11 +6,26 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant.API
 import Servant
+import Text.Megaparsec (runParser)
+import CubeParser (parseCubeState)
+import qualified Data.Text as T
+import CFOP.CFOP (cfop)
+import Control.Monad.State (evalState)
 
-type CubeSolverApi = "api" :> "cubesolver" :> Get '[PlainText] String
+type CubeSolverApi = "api" :> "cubesolver" :> QueryParam "cube" String :>  Get '[PlainText] String
+
+solveCube :: Maybe String -> Handler String
+solveCube Nothing = return "No paramter given\n"
+solveCube (Just unparsedCube) = do
+    let parsedResult = runParser parseCubeState "" (T.pack unparsedCube)
+    case parsedResult of
+        Left _ -> return "Parsing failed"
+        Right cubeState -> return $ show (evalState cfop cubeState) ++ "\n"
+            -- do
+            -- generatePDFFromSolution (evalState cfop cubeState) cubeState
 
 server :: Server CubeSolverApi
-server = return "Hello world\n"
+server = solveCube
 
 api :: Proxy CubeSolverApi
 api = Proxy
