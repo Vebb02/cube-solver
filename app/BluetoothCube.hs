73595fa012b086083bb1418cf54546743c021119
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-04-15 15:12:08 +0200

    Create scramble and solving phase

--- app/BluetoothCube.hs
+++ app/BluetoothCube.hs
@@ -1,15 +1,17 @@
 module BluetoothCube (bluetooth) where
 
-import PDFCube (generatePDFFromCubeState)
+import PDFCube (generatePDFFromSolution)
 import System.IO
 import System.Process
-import Cube (move, Move)
+import Cube (move, Move, Algorithm, combineMoves, reverseMove)
 import CubeParser (parseMove)
 import qualified Data.Text as T
 import Text.Megaparsec (runParser)
 import CubeState (CubeState, solvedCube)
-import Control.Monad.State (execState, when)
+import Control.Monad.State (execState, when, evalState)
 import Control.Concurrent (threadDelay)
+import CFOP.CFOP (cfop)
+import Data.List (minimumBy)
 
 
 bluetooth :: IO ()
@@ -35,37 +37,51 @@ bluetoothInteraction hout = do
     countDown 3
     flushOutput hout
     putStrLn "Start scramblin!"
-    scrambleCube 0 hout solvedCube
+    cubeState <- scrambleCube 0 hout solvedCube
+    solveCube hout cubeState (evalState cfop cubeState)
 
 readLines :: Int -> Handle -> IO ()
-readLines count hout
-    | count <= 0 = return ()
-    | otherwise = do
+readLines count hout =
+    if count <= 0
+    then return ()
+    else do
         _ <- hGetLine hout
         readLines (count - 1) hout
 
 countDown :: Int -> IO ()
-countDown count
-    | count <= 0 = putStrLn "0"
-    | otherwise = do
+countDown count =
+    if count <= 0
+    then return ()
+    else do
         print count
         threadDelay 1000000
         countDown (count - 1)
 
 flushOutput :: Handle -> IO ()
 flushOutput hout = do
-    isMoreToRead <- hIsEOF hout
+    isMoreToRead <- hReady hout
     when isMoreToRead $ do
         _ <- hGetLine hout
         flushOutput hout
 
-scrambleCube :: Int -> Handle -> CubeState -> IO ()
-scrambleCube 100 _ _ = return ()
+scrambleCube :: Int -> Handle -> CubeState -> IO CubeState
+scrambleCube 30 _ cubeState = return cubeState
 scrambleCube count hout cubeState = do
     m <- parseNextMove hout
+    print m
     let newCubeState = nextState m cubeState
-    generatePDFFromCubeState newCubeState
     scrambleCube (count + 1) hout newCubeState
+
+solveCube :: Handle -> CubeState -> Algorithm -> IO ()
+solveCube _ cubeState [] = generatePDFFromSolution cubeState []
+solveCube hout cubeState solution@(x:xs) = do
+    generatePDFFromSolution cubeState solution
+    m <- parseNextMove hout
+    let newCubeState = nextState m cubeState
+    let currentSolution = combineMoves (reverseMove m) x ++ xs
+    let possibleNewSolution = evalState cfop newCubeState
+    let newSolution = minimumBy (\l1 l2 -> if length l1 <= length l2 then LT else GT) [currentSolution, possibleNewSolution]
+    solveCube hout newCubeState newSolution
 
 nextState :: Move -> CubeState -> CubeState
 nextState m = execState (move m)
