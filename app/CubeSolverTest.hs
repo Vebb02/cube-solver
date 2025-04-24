c1801c997a7acfc48358994b5e79fddc28e50cb1
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-04-24 14:32:08 +0200

    Add some tests

--- app/CubeSolverTest.hs
+++ app/CubeSolverTest.hs
@@ -1,21 +1,43 @@
-module CubeSolverTest (tests) where
+module Main where
 
-import Distribution.TestSuite
+import Cube
+import CubeState
+import CubeValidator
 
-tests :: IO [Test]
-tests = return [ Test succeeds, Test fails ]
-  where
-    succeeds = TestInstance
-        { run = return $ Finished Pass
-        , name = "succeeds"
-        , tags = []
-        , options = []
-        , setOption = \_ _ -> Right succeeds
-        }
-    fails = TestInstance
-        { run = return $ Finished $ Fail "Always fails!"
-        , name = "fails"
-        , tags = []
-        , options = []
-        , setOption = \_ _ -> Right fails
-        }
+import Test.QuickCheck
+import Control.Monad.State
+
+main :: IO ()
+main = do
+    quickCheck moveChangesState
+    quickCheck alwaysValidCubeState
+
+moveChangesState :: Move -> Bool
+moveChangesState m = 
+    let newState = execState (move m) solvedCube
+    in not $ cubeSolved newState
+
+alwaysValidCubeState :: Algorithm -> Bool
+alwaysValidCubeState alg = 
+    let newState = execState (applyAlgorithm alg) solvedCube
+    in validateCubeState newState
+
+instance Arbitrary Move where
+    arbitrary = applyArbitrary2 Move 
+
+instance Arbitrary MoveFace where
+    arbitrary = 
+        oneof [ return F
+              , return B
+              , return R
+              , return L
+              , return U
+              , return D
+              ]
+
+instance Arbitrary MoveDirection where
+    arbitrary = 
+        oneof [ return Normal
+              , return Prime
+              , return Two
+              ]
